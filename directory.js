c4865384ac5230f374e15b05ab30a5a69325e308
Saundie Weiss <saundieweiss@gmail.com>
2015-11-07 11:59:09 -0700

    Created my own directory to search and find members at Galvanize.

--- directory.js
+++ directory.js
@@ -2,4 +2,31 @@ var directory = {
   jim: "Bulb",
   sara: "Leading Learners",
   ray: "Chem with Ray"
+};
+//
+// for(var key in directory){
+//   console.log(key);
+// }
+
+var input = window.prompt("Enter the name of a Galvanize Member to find the company they work for." )
+
+
+function searchGalvanize(input) {
+for(var key in directory){
+  if(input == key){
+    return directory[input];
+  }
 }
+}
+// if(input === 'jim') {
+//   return directory[input];
+//   }
+//   if(input === 'sara') {
+//   return directory[input];
+//         }
+//   if(input === 'ray') {
+//   return directory[input];
+//             }
+// }
+
+console.log(searchGalvanize('ray'));
